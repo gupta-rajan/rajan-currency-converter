ca21857a8f8ddb2625dbbaf1b149034faaf9c9d3
gupta-rajan <rjngupta22@gmail.com>
2024-07-04 14:17:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+const freecurrencyapi = new Freecurrencyapi('fca_live_9q4V4IEIpL1c2caBOVfcr5Qi7zIT73TVhZjHlLsG');
+
+export async function convertCurrency(fromCurrency, toCurrency, units){
+    const res = await freecurrencyapi.latest({
+        base_currency: fromCurrency,
+        currencies: toCurrency,
+    });
+    const multiplier = res.data[toCurrency];
+    return multiplier*units;
+}
